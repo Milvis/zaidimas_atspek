b4d3c9705b6d2bd20e2a60b75d2897ab0d514222
Milvis <info@milvis.lt>
2018-04-22 22:28:47 +0300

    pirmas zaidimo commitas

---
+++ _zaidimo_langas/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace _zaidimo_langas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(24, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(186, 210);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(246, 34);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(186, 210);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(472, 34);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(186, 210);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(24, 308);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(91, 21);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Trikampis";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(24, 384);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(102, 21);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Apskritimas";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(24, 447);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(108, 21);
+            this.checkBox3.TabIndex = 5;
+            this.checkBox3.Text = "Keturkampis";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(246, 308);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(91, 21);
+            this.checkBox4.TabIndex = 6;
+            this.checkBox4.Text = "Trikampis";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(246, 384);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(102, 21);
+            this.checkBox5.TabIndex = 7;
+            this.checkBox5.Text = "Apskritimas";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(246, 447);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(108, 21);
+            this.checkBox6.TabIndex = 8;
+            this.checkBox6.Text = "Keturkampis";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            this.checkBox6.CheckedChanged += new System.EventHandler(this.checkBox6_CheckedChanged);
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(472, 308);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(91, 21);
+            this.checkBox7.TabIndex = 9;
+            this.checkBox7.Text = "Trikampis";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(472, 384);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(102, 21);
+            this.checkBox8.TabIndex = 10;
+            this.checkBox8.Text = "Apskritimas";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBox9
+            // 
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.Location = new System.Drawing.Point(472, 447);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(111, 21);
+            this.checkBox9.TabIndex = 11;
+            this.checkBox9.Text = "Keturikampis";
+            this.checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.button1.Location = new System.Drawing.Point(664, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 52);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Spėjimas";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(40, 481);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(206, 36);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "label2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Visible = false;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.textBox1.Location = new System.Drawing.Point(427, 476);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(344, 45);
+            this.textBox1.TabIndex = 15;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(793, 526);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox9);
+            this.Controls.Add(this.checkBox8);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Atspek figura";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ _zaidimo_langas/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _zaidimo_langas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox6_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        int likospejimu = 0;
+      //  while (likospejimu<=10)
+        public void button1_Click(object sender, EventArgs e)
+        {
+            if (likospejimu==11)
+            {
+                if(MessageBox.Show("GAME OVER\nAr norite pakartoti?", "Žaidimo pabaiga", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    likospejimu = 0;
+                    textBox1.BackColor = Color.White;
+                }
+
+              //  MessageBox.Show("Pabaiga");
+            }
+            label2.Visible = false;
+            Random sk = new Random();
+
+            int randomnumber = sk.Next(1, 4);//0 ir 4 neima random skaicius nuo 1 iki 3
+                                             // randomnumber = 1;
+                                             // trikampis = 1;
+                                             // apskritimas = 2;
+                                             // keturkampis = 3;
+            int atspeta = 0;
+            //int kiek = 0;
+
+
+
+
+
+            if (randomnumber == 1)
+            {
+                pictureBox1.Image = new Bitmap("trikampis.png");
+                if (checkBox1.CheckState == CheckState.Checked)
+                {
+                    atspeta++;
+                }
+            }
+            else if (randomnumber == 2)
+            {
+                pictureBox1.Image = new Bitmap("apskritimas.png");
+                if (checkBox2.CheckState == CheckState.Checked)
+                {
+                    atspeta++;
+                }
+            }
+            else if (randomnumber == 3)
+            {
+                pictureBox1.Image = new Bitmap("keturkampis.png");
+                if (checkBox3.CheckState == CheckState.Checked)
+                {
+                    atspeta++;
+                }
+            }
+
+            //naujas random antram pav
+
+
+            int randomnumber1 = sk.Next(1, 4);//0 ir 4 neima random skaicius nuo 1 iki 3
+                                              // randomnumber1 = 1;
+            if (randomnumber1 == 1)
+            {
+                pictureBox2.Image = new Bitmap("trikampis.png");
+                if (checkBox4.CheckState == CheckState.Checked)
+                {
+                    atspeta++;
+                }
+            }
+            else if (randomnumber1 == 2)
+            {
+                pictureBox2.Image = new Bitmap("apskritimas.png");
+                if (checkBox5.CheckState == CheckState.Checked)
+                {
+                    atspeta++;
+                }
+            }
+            else if (randomnumber1 == 3)
+            {
+                pictureBox2.Image = new Bitmap("keturkampis.png");
+                if (checkBox6.CheckState == CheckState.Checked)
+                {
+                    atspeta++;
+                }
+            }
+
+            //treciam pav.
+
+
+            int randomnumber2 = sk.Next(1, 4);//0 ir 4 neima random skaicius nuo 1 iki 3
+                                              //   randomnumber2 = 1;
+            if (randomnumber2 == 1)
+            {
+                pictureBox3.Image = new Bitmap("trikampis.png");
+                if (checkBox7.CheckState == CheckState.Checked)
+                {
+                    atspeta++;
+                }
+            }
+            else if (randomnumber == 2)
+            {
+                pictureBox3.Image = new Bitmap("apskritimas.png");
+                if (checkBox8.CheckState == CheckState.Checked)
+                {
+                    atspeta++;
+                }
+            }
+            else if (randomnumber == 3)
+            {
+                pictureBox3.Image = new Bitmap("keturkampis.png");
+                if (checkBox9.CheckState == CheckState.Checked)
+                {
+                    atspeta++;
+                }
+            }
+
+
+            if (atspeta == 3)
+            {
+                label2.Visible = true;
+                label2.Text = "Atspėjote!!!";
+                label2.BackColor = Color.Green;
+
+                if (MessageBox.Show("Žaidimas laimėtas.\nAr norite pakartoti?", "Žaidimo pabaiga", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    likospejimu = 0;
+                    
+                }
+
+            }
+            else
+            {
+                label2.Visible = true;
+                label2.Text = "Neatspėjote. Bandykite dar karta";
+                label2.BackColor = Color.Red;
+            }
+
+
+            textBox1.Visible = true;
+            if (likospejimu < 10)
+            {
+                textBox1.Text = "Liko spėjimu: " + (10 - likospejimu);
+                likospejimu++;
+            }
+            else if (likospejimu == 10)
+            {
+                textBox1.Text = "Paskutinis spėjimas! ";
+                textBox1.BackColor = Color.Orange;
+                likospejimu++;
+            }
+        } 
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+       //     label2.Text = "Atspejote!!!";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+       
+        }
+    }
+}
